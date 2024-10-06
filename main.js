262d5c08f0511afca2f7584122bc9735e41da590
Amir3an <amiralisayad0019@gmail.com>
2024-10-06 22:24:24 +0330

    final Js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const body = document.querySelector("body");
   const aside = document.querySelector("aside");
+  const section = document.querySelector("section");
   const inp = document.querySelector(".inp");
   const btn = document.querySelector(".close");
+  const iconBtn = btn.getElementsByTagName("img");
+  const writtens = aside.querySelectorAll("p");
 
   function lightToDark() {
     if (inp.checked) {
@@ -17,8 +20,26 @@ document.addEventListener("DOMContentLoaded", function () {
   btn.addEventListener("click", () => {
     if (aside.style.width == "240px") {
       aside.style.width = "90px";
+      section.style.transform = "translateX(85px)";
+      for (let i = 0; i < iconBtn.length; ++i) {
+        iconBtn[0].style.transform = "rotate(180deg)";
+      }
+      section.style.opacity = "0";
+      writtens.forEach((p) => {
+        p.style.transform = "translateX(85px)";
+        p.style.opacity = "0";
+      });
     } else {
       aside.style.width = "240px";
+      section.style.transform = "translateX(0)";
+      for (let i = 0; i < iconBtn.length; ++i) {
+        iconBtn[0].style.transform = "rotate(0)";
+      }
+      section.style.opacity = "1";
+      writtens.forEach((p) => {
+        p.style.transform = "translateX(0)";
+        p.style.opacity = "1";
+      });
     }
   });
 });
